6f576883e90b20911c26c168f40d9d0f6b9d7dc3
imrahul05 <rahulkumar20000516@gmail.com>
2024-10-22 10:57:37 +0530

    Refactor function declarations and add comments for execution context

---
+++ Day7/Functio_working.js
@@ -0,0 +1,21 @@
+var x =23
+
+a()
+b()
+
+function a (){
+    var x = 10
+    console.log(x)
+}
+
+function b(){
+    var x = 100
+    console.log(x)
+}
+
+/*Execution context will be created for execution and 
+, a call stack will be generated 
+ and for a() a smaller context will created and
+  it will be put under call satck and the moment it will be executed ,
+  it will be removed and b() will be put in call satck for execution.
+*/
