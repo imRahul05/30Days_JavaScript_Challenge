f71fe51ad377bd60b177e85e0a1d756b8aa16bfb
imrahul05 <rahulkumar20000516@gmail.com>
2024-10-27 23:17:06 +0530

    add intersectionOfArray function to find the intersection of two arrays

---
+++ Day012/intercectionOfArray.js
@@ -0,0 +1,23 @@
+function intersectionOfArray(N, arr1, arr2){
+    let com = []; 
+    let str="";
+       for (let i = 0; i < N; i++) {
+           for (let j = 0; j < N; j++) {
+               if (arr1[i] === arr2[j]) {
+                   let copy = false;
+                   for (let k = 0; k < com.length; k++) {
+                       if (com[k] === arr1[i]) {
+                           copy = true; 
+                           break; 
+                       }
+                   }
+                   if (!copy) {
+                       com.push(arr1[i]); 
+                       str=arr1[i]
+                   }
+               }
+           }
+       }
+       console.log(str)
+   }
+   
