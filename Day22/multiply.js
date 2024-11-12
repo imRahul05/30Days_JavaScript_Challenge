da46b550bb9f4ef830dffebcaadb45406899bfb4
imrahul05 <rahulkumar20000516@gmail.com>
2024-11-12 17:47:31 +0530

    Fix typo in navigation and update name capitalization; correct 'Constact' to 'Contact' and change 'rahul' to 'Rahul'. Add VSCode settings for spell check and implement functions for product and series sum calculations.

---
+++ Day22/multiply.js
@@ -0,0 +1,5 @@
+function findProduct(A,B,C,D,E){
+    //write your code here
+     const product = A*B*C*D*E
+     console.log(product)
+    }
---
+++ Day22/seeries_sum.js
@@ -0,0 +1,9 @@
+function seriesSum(N){
+    //write your code here
+    let sum  = 0;
+    for(let i =1 ;i<=N;i++){
+      sum += 2*i-1;
+      
+    }
+    console.log(sum)
+  }
