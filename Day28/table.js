881079e64bc13101b09ed5568f19f34f522c00d3
imrahul05 <rahulkumar20000516@gmail.com>
2024-11-21 20:06:14 +0530

    Add multiplication table generator in table.js

---
+++ Day28/table.js
@@ -0,0 +1,26 @@
+// age = 30
+// marks = 100
+// if(age>28)
+//   console.log("Not eligible to Age")
+// else if (age>=18 && age<=28){
+//   if(marks>=70)
+//   console.log("Eligible for Admission")
+//   else
+//     console.log("marks too low")
+// }else if (age<18)
+//   console.log("Not eligible due to Age")
+
+
+// purchaseAmount = 100 , hasCoupon = true, isStudent=true, age=22
+
+// if(purchaseAmount>100){
+//     if()
+// }
+
+let n=5
+for(let j=1;j<=5;j++){
+     for(let i =1 ;i<=10;i++){
+       console.log(`${j} x ${i}  = ${j*i}`)
+     }
+  console.log('-----------------')
+}
