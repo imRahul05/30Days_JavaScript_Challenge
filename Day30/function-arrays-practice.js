952902eb531fa820921b69eebba6b67473663108
imrahul05 <rahulkumar20000516@gmail.com>
2024-11-27 15:27:02 +0530

    Refactor HTML structure and replace index.html; remove unused file and add greeting functionality with user input

--- Day30/function-arrays-practice.js
+++ Day30/function-arrays-practice.js
@@ -174,3 +174,30 @@ for(let i=0;i<fruit.length;i++){
   return -1
 }
 console.log(findIndex(fruit,target))
+
+
+
+// let n=5
+// // for(let i=1;i<=5;i++){
+// //     let bag=''
+// //     for(let j=1;j<=5;j++){
+// //         if(i===2||i===3||i===4){
+// //             bag="*"
+// //             break;
+// //         }
+// //         else
+// //         bag+="* "
+// //     }
+// //     console.log(bag)
+// // }
+
+// let n=3
+// let x=1
+// for(let i=1;i<=n;i++){
+//     let bag=''
+//     for(let j=1;j<=n;j++){
+//         bag+=x
+//         x++;
+//     }
+//     console.log(` ${bag}   \n`)
+// }
