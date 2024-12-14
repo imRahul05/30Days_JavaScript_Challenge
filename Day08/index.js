0416bb60ceaa1e6552a8ae8b54ce20c9d1b915bd
imrahul05 <rahulkumar20000516@gmail.com>
2024-12-14 11:36:55 +0530

    Refactor todo list functionality with improved display and styling

--- Day08/index.js
+++ Day08/index.js
@@ -1,145 +1,71 @@
 const input = document.getElementById("todo-input");
 const addbtn = document.getElementById("add-btn");
-const tododis =  document.getElementById("todo-list")
+const tododis = document.getElementById("todo-list")
 const arr = [];
 
-addbtn.addEventListener("click",add);
- 
+addbtn.addEventListener("click", add);
 
-function add(){
-    if(input.value=="")
+function add() {
+    if (input.value == "")
         alert("Please enter a task....");
-    else{
+    else {
         arr.push(input.value);
-        input.value=""
+        input.value = ""
         display();
     }
-
- }
-function display(){
-    tododis.innerHTML=''
-    arr.forEach((task,i)=>{
-
-
-       const li = document.createElement("li")
-       li.innerHTML= task
-
-       const checkBox = document.createElement("input");
-       checkBox.type= "checkbox"
-       checkBox.className = "check"
-       li.appendChild(checkBox)
-       checkBox.addEventListener("click",()=>strike(i))
-
-       const deleteBtn = document.createElement("button")
-       deleteBtn.innerHTML = "Delete"
-       deleteBtn.className = "btn btn-warning"
-       li.appendChild(deleteBtn)
-       deleteBtn.addEventListener("click",()=>del(i))
-       tododis.appendChild(li)
-
-
-       const editBtn = document.createElement("button")
-       editBtn.innerHTML = "Edit"
-       editBtn.className = "btn btn-danger"
-       li.appendChild(editBtn)
-       editBtn.addEventListener("click",()=>edit(i))
-
-    })
 }
 
-function del(i){
-    arr.splice(i,1)
-   display();
- }
+function display() {
+    tododis.innerHTML = ''
+    arr.forEach((task, i) => {
+        const li = document.createElement("li");
+        li.className = "todo-item";
 
-  function edit(i){
-     const nwtask = prompt("Enter task");
-     arr[i]= nwtask
-     display();
-  }
-function strike(i){
-    const li = document.querySelectorAll("li")[i]
-    li.classList.toggle("strike")
+        const checkBox = document.createElement("input");
+        checkBox.type = "checkbox";
+        checkBox.className = "check";
+        checkBox.addEventListener("click", () => strike(i));
+
+        const taskText = document.createElement("span");
+        taskText.className = "todo-text";
+        taskText.innerHTML = task;
+
+        const deleteBtn = document.createElement("button");
+        deleteBtn.innerHTML = "Delete";
+        deleteBtn.className = "btn btn-danger";
+        deleteBtn.addEventListener("click", () => del(i));
+
+        const editBtn = document.createElement("button");
+        editBtn.innerHTML = "Edit";
+        editBtn.className = "btn btn-warning";
+        editBtn.addEventListener("click", () => edit(i));
+
+        const buttonContainer = document.createElement("div");
+        buttonContainer.className = "todo-buttons";
+        buttonContainer.appendChild(editBtn);
+        buttonContainer.appendChild(deleteBtn);
+
+        li.appendChild(checkBox);
+        li.appendChild(taskText);
+        li.appendChild(buttonContainer);
+
+        tododis.appendChild(li);
+    });
+}
+
+function del(i) {
+    arr.splice(i, 1)
     display();
 }
 
+function edit(i) {
+    const nwtask = prompt("Enter task");
+    arr[i] = nwtask
+    display();
+}
 
-// const input = document.getElementById("todo-input");
-// const addbtn = document.getElementById("add-btn");
-// const tododis = document.getElementById("todo-list");
-
-// // Updated array to store task and completion state
-// const arr = [];
-
-// addbtn.addEventListener("click", add);
-
-// function add() {
-//     if (input.value == "") {
-//         alert("Please enter a task....");
-//     } else {
-//         // Push object with task text and completion state
-//         arr.push({ task: input.value, completed: false });
-//         input.value = "";
-//         display();
-//     }
-// }
-
-// function display() {
-//     tododis.innerHTML = '';
-//     arr.forEach((item, i) => {
-//         const li = document.createElement("li");
-
-//         // Set task text and apply 'strike' class if completed
-//         li.innerHTML = item.task;
-//         if (item.completed) {
-//             li.classList.add("strike");
-//         }
-
-//         // Create checkbox and set it based on the task's completion status
-//         const checkBox = document.createElement("input");
-//         checkBox.type = "checkbox";
-//         checkBox.className = "check";
-//         checkBox.checked = item.completed; // Set the checkbox state
-//         li.appendChild(checkBox);
-//         checkBox.addEventListener("click", () => toggleStrike(checkBox, li, i));
-
-//         const deleteBtn = document.createElement("button");
-//         deleteBtn.innerHTML = "Delete";
-//         deleteBtn.className = "btn btn-warning";
-//         li.appendChild(deleteBtn);
-//         deleteBtn.addEventListener("click", () => del(i));
-        
-//         const editBtn = document.createElement("button");
-//         editBtn.innerHTML = "Edit";
-//         editBtn.className = "btn btn-danger";
-//         li.appendChild(editBtn);
-//         editBtn.addEventListener("click", () => edit(i));
-
-//         tododis.appendChild(li);
-//     });
-// }
-
-// function del(i) {
-//     arr.splice(i, 1);
-//     display();
-// }
-
-// function edit(i) {
-//     const nwtask = prompt("Enter task", arr[i].task); // Prepopulate with the current task
-//     if (nwtask !== null && nwtask.trim() !== "") { // Ensure the input isn't empty or canceled
-//         arr[i].task = nwtask;
-//         display();
-//     }
-// }
-
-// function toggleStrike(checkBox, li, i) {
-//     // Toggle the completed state of the task
-//     arr[i].completed = checkBox.checked;
-
-//     // Apply or remove the 'strike' class based on the checkbox state
-//     if (checkBox.checked) {
-//         li.classList.add("strike");
-//     } else {
-//         li.classList.remove("strike");
-//     }
-// }
+function strike(i) {
+    const li = document.querySelectorAll("li")[i];
+    li.classList.toggle("strike");
+    arr[i] = li.classList.contains("strike") ? `~~${arr[i]}~~` : arr[i].replace(/~~/g, '');
+}
--- Day30/try.js
+++ Day30/try.js
@@ -1,1 +1,26 @@
-10
+function Employee(firstName, lastName, position) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.position = position;
+  
+
+  }
+  
+//   Employee.prototype.getDetails = function () {
+//     return `${this.firstName} ${this.lastName} - ${this.position}`;
+//   };
+
+
+  
+   Employee.prototype = Object.getDetails = function () {
+     return `${this.firstName} ${this.lastName} - ${this.position}`;
+   };
+
+
+
+  // Creating employee instances
+  const employee1 = new Employee("Alice", "Johnson", "Manager");
+  const employee2 = new Employee("Bob", "Smith", "Engineer");
+  
+  console.log(employee1)
+  console.log(employee2)
