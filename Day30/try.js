021aa408bfe59b7067c0867fa1801cb93263383e
imrahul05 <rahulkumar20000516@gmail.com>
2024-12-15 00:01:38 +0530

    Implement inventory function to calculate reorder costs by category

--- Day30/try.js
+++ Day30/try.js
@@ -1,26 +1,33 @@
-function Employee(firstName, lastName, position) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.position = position;
+function inventory(products) {
+  const low = products.filter(product => product.stock < 100);
   
+  const rData = low.map(product => ({ name: product.name,category: product.category,
+    reorderCost: product.pricePerUnit * (100 - product.stock),
+  }));
 
-  }
-  
-//   Employee.prototype.getDetails = function () {
-//     return `${this.firstName} ${this.lastName} - ${this.position}`;
-//   };
+  const rCost = rData.reduce((acc, product) => {
+    if (!acc[product.category]) {
+      acc[product.category] = 0;
+    }
+    acc[product.category] += product.reorderCost;
+    return acc;
+  }, {});
 
+  const srCategry = Object.entries(rCost)
+    .sort((a, b) => b[1] - a[1]) 
+    .reduce((acc, [category, cost]) => {
+      acc[category] = cost;
+      return acc;
+    }, {});
 
-  
-   Employee.prototype = Object.getDetails = function () {
-     return `${this.firstName} ${this.lastName} - ${this.position}`;
-   };
+  return srCategry;
+}
+const products = [{ name: "Laptop", category: "Electronics", stock: 50, pricePerUnit: 1000 },
+  { name: "Phone", category: "Electronics", stock: 150, pricePerUnit: 500 },
+  { name: "T-shirt", category: "Clothing", stock: 40, pricePerUnit: 20 },
+  { name: "Jeans", category: "Clothing", stock: 90, pricePerUnit: 40 },
+  { name: "Watch", category: "Accessories", stock: 70, pricePerUnit: 150 },
+];
 
-
-
-  // Creating employee instances
-  const employee1 = new Employee("Alice", "Johnson", "Manager");
-  const employee2 = new Employee("Bob", "Smith", "Engineer");
-  
-  console.log(employee1)
-  console.log(employee2)
+const result = inventory(products);
+console.log(result);
